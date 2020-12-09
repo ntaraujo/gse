7ee5c3aaa6f016930781a5d6d582d2bde1660b1e
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2020-12-09 02:40:45 -0300

    New design, main windows & file chooser

--- main.py
+++ main.py
@@ -1,26 +1,66 @@
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.filemanager import MDFileManager
+from kivymd.toast import toast
+from kivy.core.window import Window
+import os
 from gse import Process
-from kivy.uix.label import Label
+
 
 class Manager(ScreenManager):
     pass
 
-class Welcome(Screen):
+
+class Welcome(MDScreen):
     pass
 
-class Background(Screen):
+
+class Background(MDScreen):
     pass
 
-class Time(Screen):
+
+class Time(MDScreen):
     pass
 
-class Advanced(Screen):
+
+class Advanced(MDScreen):
     pass
 
-class GSE(App):
+
+class GSE(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.manager_open = False
+        Window.bind(on_keyboard=self.events)
+        self.file_manager = MDFileManager(
+            exit_manager=self.exit_manager,
+            select_path=self.select_path,
+        )
+
+    def file_manager_open(self):
+        self.file_manager.show(os.path.abspath(os.getcwd()))  # output manager to the screen
+        self.manager_open = True
+
+    def select_path(self, path):
+        self.exit_manager()
+        toast(path)
+
+    def exit_manager(self, *args):
+        self.manager_open = False
+        self.file_manager.close()
+
+    def events(self, instance, keyboard, keycode, text, modifiers):
+        if keyboard in (1001, 27):
+            if self.manager_open:
+                self.file_manager.back()
+        return True
+
     def build(self):
-        return Label(text="roi")
+        self.theme_cls.primary_palette = "LightGreen"
+        # self.theme_cls.theme_style = "Dark"
+        return Manager()
+
 
 if __name__ == '__main__':
     GSE().run()
