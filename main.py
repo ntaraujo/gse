61f6285bfbe24d3b2a6b0ecc03a5a1aa967f017e
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2020-12-18 18:32:30 -0300

    bring functions to right place in advanced

--- main.py
+++ main.py
@@ -29,7 +29,95 @@ class Time(MDScreen):
 
 
 class Advanced(MDScreen):
-    pass
+    mask_menu = scaler_menu = compression_menu = video_codec_menu = audio_codec_menu = None
+    video_codec_menu_items = [{"text": i} for i in ["default", "libx264 (.mp4)", "mpeg4 (.mp4)", "rawvideo (.avi)",
+                                                    "png (.avi)", "libvorbis (.ogv)", "libvpx (.webm)"]]
+    audio_codec_menu_items = [{"text": i} for i in ["default", "libmp3lame (.mp3)", "libvorbis (.ogg)",
+                                                    "libfdk_aac (.m4a)", "pcm_s16le (.wav)", "pcm_s32le (.wav)"]]
+    compression_menu_items = [{"text": i} for i in ["ultrafast", "superfast", "veryfast", "faster", "fast",
+                                                    "medium", "slow", "slower", "veryslow", "placebo"]]
+    scaler_menu_items = [{"text": i} for i in ["fast_bilinear", "bilinear", "bicubic", "experimental", "neighbor",
+                                               "area", "bicublin", "gauss", "sinc", "lanczos", "spline"]]
+    mask_menu_items = [{"text": i} for i in ["A.I.", "Video/Image"]]
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.video_codec_menu = MDDropdownMenu(
+            caller=self.ids.video_codec_button,
+            items=self.video_codec_menu_items,
+            width_mult=4,
+        )
+        self.video_codec_menu.bind(on_release=self.video_codec_menu_callback)
+
+        self.audio_codec_menu = MDDropdownMenu(
+            caller=self.ids.audio_codec_button,
+            items=self.audio_codec_menu_items,
+            width_mult=4,
+        )
+        self.audio_codec_menu.bind(on_release=self.audio_codec_menu_callback)
+
+        self.compression_menu = MDDropdownMenu(
+            caller=self.ids.compression_button,
+            items=self.compression_menu_items,
+            width_mult=4,
+        )
+        self.compression_menu.bind(on_release=self.compression_menu_callback)
+
+        self.scaler_menu = MDDropdownMenu(
+            caller=self.ids.scaler_button,
+            items=self.scaler_menu_items,
+            width_mult=4,
+        )
+        self.scaler_menu.bind(on_release=self.scaler_menu_callback)
+
+        self.mask_menu = MDDropdownMenu(
+            caller=self.ids.mask_button,
+            items=self.mask_menu_items,
+            width_mult=4,
+        )
+        self.mask_menu.bind(on_release=self.mask_menu_callback)
+
+    def video_codec_menu_callback(self, instance_menu, instance_menu_item):
+        if "default" in instance_menu_item.text:
+            app.ctrl.video_codec = None
+        else:
+            app.ctrl.video_codec = instance_menu_item.text.split()[0]
+        instance_menu.caller.text = instance_menu_item.text
+        instance_menu.dismiss()
+
+    def audio_codec_menu_callback(self, instance_menu, instance_menu_item):
+        if "default" in instance_menu_item.text:
+            app.ctrl.audio_codec = None
+        else:
+            app.ctrl.audio_codec = instance_menu_item.text.split()[0]
+        instance_menu.caller.text = instance_menu_item.text
+        instance_menu.dismiss()
+
+    def compression_menu_callback(self, instance_menu, instance_menu_item):
+        app.ctrl.compression = instance_menu.caller.text = instance_menu_item.text
+        instance_menu.dismiss()
+
+    def scaler_menu_callback(self, instance_menu, instance_menu_item):
+        app.ctrl.scaler = instance_menu.caller.text = instance_menu_item.text
+        instance_menu.dismiss()
+
+    def mask_menu_callback(self, instance_menu, instance_menu_item):
+        if instance_menu_item.text == "A.I.":
+            app.ctrl.mask = ""
+            instance_menu.caller.text = "A.I."
+        else:
+            app.file_manager_open()
+            app.go_to = ["advanced"]
+        instance_menu.dismiss()
+
+    def threads_button(self, up):
+        if up:
+            app.ctrl.threads += 1
+        else:
+            app.ctrl.threads -= 1
+        self.ids.threads_label.text = f"{app.ctrl.threads} threads"
+
+
 
 
 class Ready(MDScreen):
@@ -77,15 +165,6 @@ class GSE(MDApp):
     sm = mask_menu = scaler_menu = compression_menu = video_codec_menu = audio_codec_menu = advanced = None
     go_to = ["welcome"]
     ctrl = Control()
-    video_codec_menu_items = [{"text": i} for i in ["default", "libx264 (.mp4)", "mpeg4 (.mp4)", "rawvideo (.avi)",
-                                                    "png (.avi)", "libvorbis (.ogv)", "libvpx (.webm)"]]
-    audio_codec_menu_items = [{"text": i} for i in ["default", "libmp3lame (.mp3)", "libvorbis (.ogg)",
-                                                    "libfdk_aac (.m4a)", "pcm_s16le (.wav)", "pcm_s32le (.wav)"]]
-    compression_menu_items = [{"text": i} for i in ["ultrafast", "superfast", "veryfast", "faster", "fast",
-                                                    "medium", "slow", "slower", "veryslow", "placebo"]]
-    scaler_menu_items = [{"text": i} for i in ["fast_bilinear", "bilinear", "bicubic", "experimental", "neighbor",
-                                               "area", "bicublin", "gauss", "sinc", "lanczos", "spline"]]
-    mask_menu_items = [{"text": i} for i in ["A.I.", "Video/Image"]]
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -103,39 +182,6 @@ class GSE(MDApp):
             self.ctrl.extension = strs[1]
         else:
             pass  # error
-
-    def video_codec_menu_callback(self, instance_menu, instance_menu_item):
-        if "default" in instance_menu_item.text:
-            self.ctrl.video_codec = None
-        else:
-            self.ctrl.video_codec = instance_menu_item.text.split()[0]
-        instance_menu.caller.text = instance_menu_item.text
-        instance_menu.dismiss()
-
-    def audio_codec_menu_callback(self, instance_menu, instance_menu_item):
-        if "default" in instance_menu_item.text:
-            self.ctrl.audio_codec = None
-        else:
-            self.ctrl.audio_codec = instance_menu_item.text.split()[0]
-        instance_menu.caller.text = instance_menu_item.text
-        instance_menu.dismiss()
-
-    def compression_menu_callback(self, instance_menu, instance_menu_item):
-        self.ctrl.compression = instance_menu.caller.text = instance_menu_item.text
-        instance_menu.dismiss()
-
-    def scaler_menu_callback(self, instance_menu, instance_menu_item):
-        self.ctrl.scaler = instance_menu.caller.text = instance_menu_item.text
-        instance_menu.dismiss()
-
-    def mask_menu_callback(self, instance_menu, instance_menu_item):
-        if instance_menu_item.text == "A.I.":
-            self.ctrl.mask = ""
-            instance_menu.caller.text = "A.I."
-        else:
-            self.file_manager_open()
-            self.go_to = ["advanced"]
-        instance_menu.dismiss()
 
     def file_manager_open(self):
         # parent = os.path.dirname(os.path.abspath(os.getcwd()))
@@ -190,14 +236,6 @@ class GSE(MDApp):
     def monitor_radio(self, inst):
         if inst.active:
             self.ctrl.monitor = inst.op
-
-    def threads_button(self, up):
-        if up:
-            self.ctrl.threads += 1
-        else:
-            self.ctrl.threads -= 1
-        self.advanced.ids.threads_label.text = f"{self.ctrl.threads} threads"
-
     def build(self):
         self.theme_cls.primary_palette = "LightGreen"
         # self.theme_cls.theme_style = "Dark"
@@ -209,41 +247,6 @@ class GSE(MDApp):
         self.sm.add_widget(Ready())
         self.advanced = Advanced()
         self.sm.add_widget(self.advanced)
-
-        self.video_codec_menu = MDDropdownMenu(
-            caller=self.advanced.ids.video_codec_button,
-            items=self.video_codec_menu_items,
-            width_mult=4,
-        )
-        self.video_codec_menu.bind(on_release=self.video_codec_menu_callback)
-
-        self.audio_codec_menu = MDDropdownMenu(
-            caller=self.advanced.ids.audio_codec_button,
-            items=self.audio_codec_menu_items,
-            width_mult=4,
-        )
-        self.audio_codec_menu.bind(on_release=self.audio_codec_menu_callback)
-
-        self.compression_menu = MDDropdownMenu(
-            caller=self.advanced.ids.compression_button,
-            items=self.compression_menu_items,
-            width_mult=4,
-        )
-        self.compression_menu.bind(on_release=self.compression_menu_callback)
-
-        self.scaler_menu = MDDropdownMenu(
-            caller=self.advanced.ids.scaler_button,
-            items=self.scaler_menu_items,
-            width_mult=4,
-        )
-        self.scaler_menu.bind(on_release=self.scaler_menu_callback)
-
-        self.mask_menu = MDDropdownMenu(
-            caller=self.advanced.ids.mask_button,
-            items=self.mask_menu_items,
-            width_mult=4,
-        )
-        self.mask_menu.bind(on_release=self.mask_menu_callback)
 
         self.change()
 
