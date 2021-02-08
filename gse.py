7d8687d4f137fab93be9bd47f51d1a7515c351d2
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2021-02-08 20:45:46 -0300

    introduce new api

--- gse.py
+++ gse.py
@@ -7,10 +7,16 @@ from torchvision.transforms import Compose, Normalize
 from json import load as jload
 from json import dump as jdump
 from moviepy.video.fx.loop import loop
+from moviepy.video.fx.resize import resize
 from time import perf_counter
 from imghdr import what as is_image
 from dill import dump as ddump
 from dill import load as dload
+from os.path import dirname, basename, splitext, abspath
+from os.path import join as join_path
+from ast import literal_eval
+from typing import Union, Optional, Callable, Any, IO, Iterable
+from os import PathLike
 
 
 class MakeMask:
@@ -66,6 +72,168 @@ class MakeMask:
         newimg = (combined_mask * 255.0).cpu().squeeze().byte().permute(1, 2, 0).numpy()
 
         return newimg
+
+
+ClipType = Union[VideoFileClip, ImageClip]
+PathType = Union[str, bytes, PathLike]
+
+
+def get_input_clip(path: PathType, **videofileclip_args) -> ClipType:
+    if is_image(path):
+        print(f"Loading {path} as the main image source")
+        return ImageClip(path, duration=1).set_fps(1)
+    else:
+        print(f"Loading {path} as the main video source")
+        return VideoFileClip(path, **videofileclip_args)
+
+
+def get_mask_clip(input_clip: ClipType, relative_mask_fps: int = 100, relative_mask_resolution: int = 100,
+                  mask_path: PathType = "", cuda: bool = True, **videofileclip_args) -> ClipType:
+    if mask_path != "":  # if given
+        if is_image(mask_path):
+            print(f"Loading the image {mask_path} as the mask for {input_clip.filename}")
+            return ImageClip(mask_path, duration=input_clip.duration)
+        else:
+            print(f"Loading the video {mask_path} as the mask for {input_clip.filename}")
+            return VideoFileClip(mask_path, **videofileclip_args) \
+                .fx(loop, duration=input_clip.duration).set_duration(input_clip.duration)
+    else:  # if should be result of A.I.
+        process_clip = input_clip.copy()
+        fps = relative_mask_fps * 0.01
+        if fps != 1:  # if asked to change fps
+            newfps = input_clip.fps * fps
+            process_clip = process_clip.set_fps(newfps)
+            print(f"Mask fps decreased in {(1 - fps) * 100}%. {process_clip.fps}fps now")
+        res = relative_mask_resolution * 0.01
+        if res != 1:  # if asked to resize
+            process_clip = process_clip.fx(resize, res)
+            w, h = process_clip.size
+            print(f"Mask resolution decreased in {(1 - res) * 100}%, {w}x{h} now")
+        return process_clip.fl_image(MakeMask(cuda))
+
+
+def get_final_clip(mask_clip: ClipType, input_clip: ClipType, background: Union[list, PathType],
+                   **videofileclip_args) -> Union[ClipType, CompositeVideoClip]:
+    if background != "":
+        usable_mask = mask_clip.fx(resize, input_clip.size).to_mask()
+        masked_clip = input_clip.set_mask(usable_mask)
+        if type(background) == list:  # if color
+            rgb = (background[0], background[1], background[2])
+            print(f"Using the RGB color {rgb} as the background of {input_clip.filename}")
+            return masked_clip.on_color(color=rgb)
+        elif is_image(background):
+            print(f"Using {background} as image source to the background of {input_clip.filename}")
+            background_clip = ImageClip(background, duration=masked_clip.duration)
+            return smooth_composite(background_clip, masked_clip)
+        else:
+            print(f"Using {background} as video source to the background of {input_clip.filename}")
+            background_clip = VideoFileClip(background, **videofileclip_args) \
+                .fx(loop, duration=masked_clip.duration).set_duration(input_clip.duration)
+            return smooth_composite(background_clip, masked_clip)
+    else:
+        print("No background selected, skipping compositing")
+        return mask_clip
+
+
+def smooth_composite(back: ClipType, front: ClipType):
+    wf, hf = front.size
+    wb, hb = back.size
+    rf = wf / hf
+    rb = wb / hb
+    if rf > rb:
+        back = back.fx(resize, width=wf)
+    else:
+        back = back.fx(resize, height=hf)
+    to_return = CompositeVideoClip([back, front.set_position("center")], size=front.size)
+    to_return.filename = front.filename
+    return to_return
+
+
+def save_to_file(clip: ClipType, path: PathType, frame_from_time: int = 0, frame: int = 0,
+                 alpha: bool = False, **write_videofile_args):
+    if is_image(clip.filename) or frame or frame_from_time:
+        if frame:
+            frame_from_time = clip.fps / frame
+        elif not frame_from_time:
+            frame_from_time = clip.duration / 2
+        print(f'Saving as image to {path}')
+        clip.save_frame(path, t=frame_from_time, withmask=alpha)
+    else:
+        temp_audiofile = abspath(join_path(dirname(path), splitext(basename(path))[0] + '.mp3'))
+        print(f'Saving as video to {path}')
+        clip.write_videofile(path, temp_audiofile=temp_audiofile, **write_videofile_args)
+
+
+class Project:
+    def __init__(self, config: Optional[PathType] = None):
+        self.input_clip = self.mask_clip = self.final_clip = None
+        self.audio = True
+        if config:
+            self.load(config)
+
+    def var(self, var: str, converter: Union[type, str] = str, asker: Callable[[Any], Any] = input) -> Any:
+        if var in self.__dict__.keys():
+            to_return = self.__dict__[var]
+        else:
+            if asker == input:
+                to_return = input(f'Variable {var}: ')
+            else:
+                to_return = asker(var)
+        if converter == "auto":
+            try:
+                return literal_eval(to_return)
+            except ValueError:
+                return to_return
+        else:
+            return converter(to_return)
+
+    @staticmethod
+    def serialize(obj):
+        obj_type = str(type(obj))
+        print(f'Object of type {obj_type} was skipped')
+        return obj_type
+
+    def save(self, path: Union[IO[str], PathType]) -> None:
+        file_type = splitext(path)[1]
+        with open(f'{path}', "wb") as project_file:
+            if file_type == "gse":
+                ddump(self, project_file)
+            elif file_type == "json":
+                jdump(self.__dict__, path, default=self.serialize)
+            else:
+                raise Exception(f'Impossible to load file with extension {file_type}. Accepted: "gse" and "json"')
+        print(f'Saved to {path}\n{self.__dict__}')
+
+    def load(self, path: PathType):
+        file_type = splitext(path)[1]
+        with open(path, "rb") as project_file:
+            if file_type == "gse":
+                self.__dict__.update(dload(project_file).__dict__)
+            elif file_type == "json":
+                self.__dict__.update(jload(project_file))
+            else:
+                raise Exception(f'Impossible to load file with extension {file_type}. Accepted: "gse" and "json"')
+        print(f'Loaded from {path}\n{self.__dict__}')
+
+    def processes(self, processes: Iterable[int] = range(4)):
+        if 0 in processes:
+            self.input_clip = get_input_clip(self.var("input"), resize_algorithm=self.var("scaler"))
+        if 1 in processes:
+            self.mask_clip = get_mask_clip(self.input_clip, self.var("relative_mask_fps", int),
+                                           self.var("relative_mask_resolution", int), self.var("mask"),
+                                           self.var("cuda", bool), resize_algorithm=self.var("scaler"))
+        if 2 in processes:
+            if self.var("background", "auto") == "":
+                self.audio = False
+            self.final_clip = get_final_clip(self.mask_clip, self.input_clip,
+                                             self.var("background", "auto"),
+                                             resize_algorithm=self.var("scaler"))
+        if 3 in processes:
+            file = '.'.join([self.var("output_name"), self.var("extension")])
+            path = abspath(join_path(self.var("output_dir"), file))
+            save_to_file(self.input_clip, path, frame=self.var("get_frame", int), codec=self.var("video_codec"),
+                         audio=self.audio, preset=self.var("compression"), audio_codec=self.var("audio_codec"),
+                         write_logfile=self.var("log", bool), threads=self.var("threads", int))
 
 
 class Process:
@@ -245,10 +413,9 @@ class Timer:
 
 
 if __name__ == '__main__':
-
     t = Timer()
 
-    p = Process("config.json")
-    p.all()
+    p = Project("config.json")
+    p.processes()
 
     t.finish()
