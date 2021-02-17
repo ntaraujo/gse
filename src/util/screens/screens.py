a4118f6ab3136e6f3b075310a3b1d78fabd67be7
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2021-02-17 17:14:47 -0300

    took some screens in kv lang to another files

--- src/util/screens/screens.py
+++ src/util/screens/screens.py
@@ -1,4 +1,7 @@
 from kivymd.uix.screen import MDScreen
+from kivy.lang.builder import Builder
+from os.path import dirname, abspath, join
+from glob import glob
 
 
 class Welcome(MDScreen):
@@ -19,3 +22,10 @@ class Time(MDScreen):
 
 class Ready(MDScreen):
     pass
+
+
+this_dir = dirname(abspath(__file__))
+kvs = glob(join(this_dir, '*.kv'))
+
+for kv in kvs:
+    Builder.load_file(kv)
