5df0fca4e481968446bf2a302083313d65406847
Nathan Araújo <66187211+ntaraujo@users.noreply.github.com>
2021-03-10 10:16:56 -0300

    improvements in mythreading.py
    default_exc_callback with better print, fix of possible error when 'target' is None.
    
    in MyThread:
    get_id() and stop() functions;
    use of already implemented Thread variables instead of creating new ones;
    protected variable _exc_callback instead of exc_callback

--- src/util/mythreading.py
+++ src/util/mythreading.py
@@ -1,51 +1,63 @@
-from threading import Thread
+from threading import Thread, _active  # noqa
 from traceback import format_exc
 from typing import Callable, Optional, Any, Iterable, Mapping
+from ctypes import pythonapi, py_object
 
 
-target_type = Callable[[Any], None]
-kwargs_type = Mapping[str, Any]
-
-
-class MyThread(Thread):
-
-    def __init__(self, target: target_type, args: Optional[Iterable] = None, kwargs: Optional[kwargs_type] = None,
-                 exc_callback: Callable[[target_type, Iterable, kwargs_type, str], None] = None,
-                 daemon: bool = True, *t_args, **t_kwargs):
-        """
-        Thread but handle exceptions with exc_callback
-
-        :param target: function will run with MyThread.start
-        :param args: arguments to target
-        :param kwargs: key word arguments to target
-        :param exc_callback: function receiving target, args, kwargs and the traceback of any error
-        :param daemon: stop thread when code finishes?
-        :param t_args: arguments for Thread
-        :param t_kwargs: key word arguments for Thread
-        """
-        super().__init__(daemon=daemon, *t_args, **t_kwargs)
-        self.target = target
-        self.args = args if args else tuple()
-        self.kwargs = kwargs if kwargs else dict()
-        self.exc_callback = exc_callback if exc_callback else default_exc_callback
-
-    def run(self):
-        try:
-            self.target(*self.args, **self.kwargs)
-        except Exception:
-            traceback = format_exc()
-            self.exc_callback(self.target, self.args, self.kwargs, traceback)
-
-
-def default_exc_callback(target: target_type, args: Iterable, kwargs: kwargs_type, traceback: str) -> None:
+def default_exc_callback(target: Callable[[Any], None], args: Iterable,
+                         kwargs: Mapping[str, Any], traceback: str, e: Exception):
     """
     Default exception callback for MyThread. Pretty print the error and where happened
     """
-    func = f'{target.__name__}('
+    func = f'{target.__name__ if hasattr(target, "__name__") else target}('
     for a in args:
         func += f'{a}, '
     for k, a in kwargs.items():
         func += f'{k}={a}, '
     func += ')'
     func = func.replace(', )', ')')
-    print(f"Exception in {target}\n\n{func}\n\n{traceback}")
+    print('-' * 40)
+    print(f"{type(e).__name__} in {target}\n\n{func}\n\n{traceback}")
+    print('-' * 40)
+
+
+class MyThread(Thread):
+
+    def __init__(self, exc_callback=default_exc_callback, daemon: bool = True, *args, **kwargs):
+        """
+        Thread but handle exceptions with exc_callback and have get_id() and stop() functions
+
+        :param exc_callback: function receiving target, args, kwargs, the traceback and Exception of any error
+        :param daemon: stop thread when code finishes?
+        :param args: "target" is needed
+        """
+        self._args = ()
+        self._kwargs = {}
+        super().__init__(daemon=daemon, *args, **kwargs)
+        self._exc_callback = exc_callback
+
+    def _target(self, *args, **kwargs):
+        pass
+
+    def run(self):
+        try:
+            self._target(*self._args, **self._kwargs)
+        except Exception as e:
+            self._exc_callback(self._target, self._args, self._kwargs, format_exc(), e)
+
+    def get_id(self) -> Optional[int]:
+        """Return the Thread ID"""
+        if hasattr(self, '_thread_id'):
+            return self._thread_id
+        for thread_id, thread in _active.items():
+            if thread is self:
+                return thread_id
+
+    def stop(self):
+        """Try to raise an exception in running thread to stop it"""
+        thread_id = self.get_id()
+        res = pythonapi.PyThreadState_SetAsyncExc(thread_id, py_object(SystemExit))
+        if res > 1:
+            pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
+            self._exc_callback(self._target, self._args, self._kwargs,
+                               ".stop() call to MyThread failed", Exception())
